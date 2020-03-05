88a3edc6709a9df229165796207265bed868a252
bogdibota <bogdibota@gmail.com>
2020-03-05 13:50:12 +0200

    further isolate the issue

--- README.md
+++ README.md
@@ -10,23 +10,25 @@ npm install
 npm run start
 ```
 
-Then, open the developer tools window and you'll see the warning:
+Then, open the developer tools window and you'll **sometimes** see the warning:
 
 ```
-5index.js:1 Warning: Cannot update a component from inside the function body of a different component.
-    in CollectionDetails (at src/index.tsx:104)
-    in div (at src/index.tsx:103)
-    in CollectionInfo (at src/index.tsx:113)
-    in div (at src/index.tsx:112)
-    in ul (at src/index.tsx:110)
-    in Collections (at src/index.tsx:122)
-    in ApolloProvider (at src/index.tsx:121)
+index.js:1 Warning: Cannot update a component from inside the function body of a different component.
+    in InnerComponent (at src/index.tsx:41)
+    in WrapperComponent (at src/index.tsx:52)
+    in ApolloProvider (at src/index.tsx:46)
 ```
+
+Increasing the number of `WrapperComponent` will also increase the chance of the warning:
+ - 1 component will NEVER throw a warning
+ - 2 component will SOMETIMES throw a warning 
+ - 3 component will ALWAYS throw 1 to 3 warnings 
 
 There are a number of ways to make this error disappear:
 
-1. Remove the "if (loading)" check on line 100 in src/index.tsx.
-2. remove the part of the query for "requestDetails"
-3. reduce the number of collection items in that hard coded object.
+1. Remove the `if (loading)` check on line 37 in src/index.tsx.
+2. The unique values of the `WrapperComponent` are set to the same value
+3. Change `requestDetails` node to return the same value (server side)
+4. Setting `fetchPolicy: 'network-only'` in the `InnerComponent`'s query
 
-I don't know why these solutions work, I'm pretty baffled at the cause of this error.
+From this I would infer that updates caused by cache changes are the cause of the warning.
--- src/index.tsx
+++ src/index.tsx
@@ -1,127 +1,57 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import gql from "graphql-tag";
-import { ApolloClient } from "apollo-client";
-import { HttpLink } from "apollo-link-http";
-import { InMemoryCache } from "apollo-cache-inmemory";
-
-import {
-  useQuery,
-  ApolloProvider as ApolloHooksProvider
-} from "@apollo/react-hooks";
+import { ApolloProvider as ApolloHooksProvider, useQuery } from '@apollo/react-hooks';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import gql from 'graphql-tag';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: "/graphql"
-  })
+    cache: new InMemoryCache(),
+    link: new HttpLink({
+        uri: '/graphql',
+    }),
 });
 
-// Removing "requestDetails" from here will make the React warnings go away.
-const GET_MOVIES = gql(`
-query GetMovies($movieIds: [Int!]!) {
-  movies(movieIds: $movieIds) {
-    movieId
-    internalTitle
-  }
+const MY_QUERY = gql(`
+query MyQuery {
   requestDetails {
       id
   }
 }
 `);
 
-const collections = Object.values({
-    "161e2aec-99cd-45ba-90f7-cbe1449ddc06": {
-      "movieIds": [
-        70000794,
-        80117456
-      ]
-    },
-    "e644ab32-4ae4-4bd2-8117-7a2bccf2fb0d": {
-      "movieIds": [
-        80117715
-      ]
-    },
-    "44b68af6-9436-4111-9a5f-c8a79b4dca58": {
-      "movieIds": [
-        80117456
-      ]
-    },
-    "e128a262-036d-4133-bb20-834b07800a56": {
-      "movieIds": [
-        80117456,
-        80025678
-      ]
-    },
-    "c91bae38-82d1-4e7e-85d7-bb12bf643f73": {
-      "movieIds": [
-        80077209
-      ]
-    },
-    "365906b8-5a8a-4769-b185-abd162e685b1": {
-      "movieIds": [
-        81023181
-      ]
-    },
-    "19096da2-2138-4724-a141-0d4bf803d1dd": {
-      "movieIds": [
-        81023035
-      ]
-    },
-    "73f6d28b-bfe7-44bf-91d4-452410137618": {
-      "movieIds": [
-        81023035
-      ]
-    },
-    "8e09c04e-a0e0-46c9-bcaa-c377a8146163": {
-      "movieIds": [
-        81023035
-      ]
-    },
-    "7985704a-3b4a-4739-a814-e0513246ccd3": {
-      "movieIds": [
-        81023035
-      ]
+function InnerComponent(props: { someUniqueValue: number }) {
+    const { data } = useQuery(MY_QUERY, {
+        variables: { key: props.someUniqueValue },
+        // fetchPolicy: 'network-only',
+    });
+    console.log(`[InnerComponent ${ props.someUniqueValue }]`, data && data.requestDetails.id);
+    return <p>{ JSON.stringify(data) }</p>;
+}
+
+function WrapperComponent(props: { someUniqueValue: number }) {
+    const { loading } = useQuery(MY_QUERY, {
+        variables: { key: props.someUniqueValue },
+    });
+    // Removing this check will make the React warnings go away.
+    if (loading) {
+        return <p>loading</p>;
     }
-  });
-
-function CollectionDetails(props: { movieIds: number[] }) {
-    const { data } = useQuery(GET_MOVIES, {
-        variables: { movieIds: props.movieIds },
-        skip: false,
-      });
-    return <p>{JSON.stringify(data)}</p>
-}
-
-function CollectionInfo(props: { collection: (typeof collections)[0] }) {
-  const { loading } = useQuery(GET_MOVIES, {
-    variables: { movieIds: props.collection.movieIds },
-    skip: false,
-  })
-  // Removing this check will make the React warnings go away.
-  if (loading) {
-      return <p>loading</p>;
-  }
-  return <div>
-      <CollectionDetails movieIds={props.collection.movieIds} />
-      </div>;
-}
-
-function Collections() {
-  return (
-    <ul>
-      {collections.map((collection, index) => {
-          return <div key={index}>
-              <CollectionInfo collection={collection} />
-              </div>;
-      })}
-    </ul>
-  );
+    return <>
+        <InnerComponent someUniqueValue={ props.someUniqueValue }/>
+    </>;
 }
 
 ReactDOM.render(
-  <ApolloHooksProvider client={client}>
-    <Collections />
-  </ApolloHooksProvider>,
-  document.getElementById("root")
+    <ApolloHooksProvider client={ client }>
+        {/*There is no error with one children. The more you add, the higher the chance*/ }
+        {/*1 component: no warning*/ }
+        {/*2 components: sometimes it gives a warning*/ }
+        {/*3 components: random 1 to 3 warnings*/ }
+        <WrapperComponent someUniqueValue={ 1 }/>
+        <WrapperComponent someUniqueValue={ 2 }/>
+        <WrapperComponent someUniqueValue={ 3 }/>
+    </ApolloHooksProvider>,
+    document.getElementById('root'),
 );
