8b4c7fda737f2b0f360883e237ef4660d27b7ca5
Hugo Jobling <hello@thisishugo.com>
2020-03-10 19:08:08 +0000

    Update example to demonstrate issue with URQL

--- README.md
+++ README.md
@@ -14,21 +14,20 @@ Then, open the developer tools window and you'll **sometimes** see the warning:
 
 ```
 index.js:1 Warning: Cannot update a component from inside the function body of a different component.
-    in InnerComponent (at src/index.tsx:41)
-    in WrapperComponent (at src/index.tsx:52)
-    in ApolloProvider (at src/index.tsx:46)
+  in InnerComponent (at src/index.tsx:42)
+  in WrapperComponent (at src/index.tsx:54)
 ```
 
 Increasing the number of `WrapperComponent` will also increase the chance of the warning:
  - 1 component will NEVER throw a warning
- - 2 component will SOMETIMES throw a warning 
- - 3 component will ALWAYS throw 1 to 3 warnings 
+ - 2 component will SOMETIMES throw a warning
+ - 3 component will ALWAYS throw 1 to 3 warnings
 
 There are a number of ways to make this error disappear:
 
 1. Remove the `if (loading)` check on line 37 in src/index.tsx.
 2. The unique values of the `WrapperComponent` are set to the same value
 3. Change `requestDetails` node to return the same value (server side)
-4. Setting `fetchPolicy: 'network-only'` in the `InnerComponent`'s query
+4. Setting `networkPolicy: 'network-only'` in the `InnerComponent`'s query
 
 From this I would infer that updates caused by cache changes are the cause of the warning.
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,9 @@
-import { ApolloProvider as ApolloHooksProvider, useQuery } from '@apollo/react-hooks';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloClient } from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
-import gql from 'graphql-tag';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createClient, useQuery, Provider } from "urql";
+import gql from "graphql-tag";
+import React from "react";
+import ReactDOM from "react-dom";
 
-const client = new ApolloClient({
-    cache: new InMemoryCache(),
-    link: new HttpLink({
-        uri: '/graphql',
-    }),
-});
-
-const MY_QUERY = gql(`
+const query = gql(`
 query MyQuery {
   requestDetails {
       id
@@ -21,37 +11,48 @@ query MyQuery {
 }
 `);
 
+const client = createClient({
+  url: "http://localhost:4000/graphql"
+});
+
 function InnerComponent(props: { someUniqueValue: number }) {
-    const { data } = useQuery(MY_QUERY, {
-        variables: { key: props.someUniqueValue },
-        // fetchPolicy: 'network-only',
-    });
-    console.log(`[InnerComponent ${ props.someUniqueValue }]`, data && data.requestDetails.id);
-    return <p>{ JSON.stringify(data) }</p>;
+  const [{ data }] = useQuery({
+    query,
+    variables: { key: props.someUniqueValue }
+    // requestPolicy: 'network-only'
+  });
+  console.log(
+    `[InnerComponent ${props.someUniqueValue}]`,
+    data && data.requestDetails.id
+  );
+  return <p>{JSON.stringify(data)}</p>;
 }
 
 function WrapperComponent(props: { someUniqueValue: number }) {
-    const { loading } = useQuery(MY_QUERY, {
-        variables: { key: props.someUniqueValue },
-    });
-    // Removing this check will make the React warnings go away.
-    if (loading) {
-        return <p>loading</p>;
-    }
-    return <>
-        <InnerComponent someUniqueValue={ props.someUniqueValue }/>
-    </>;
+  const [{ fetching }] = useQuery({
+    query,
+    variables: { key: props.someUniqueValue }
+  });
+  // Removing this check will make the React warnings go away.
+  if (fetching) {
+    return <p>loading</p>;
+  }
+  return (
+    <>
+      <InnerComponent someUniqueValue={props.someUniqueValue} />
+    </>
+  );
 }
 
 ReactDOM.render(
-    <ApolloHooksProvider client={ client }>
-        {/*There is no error with one children. The more you add, the higher the chance*/ }
-        {/*1 component: no warning*/ }
-        {/*2 components: sometimes it gives a warning*/ }
-        {/*3 components: random 1 to 3 warnings*/ }
-        <WrapperComponent someUniqueValue={ 1 }/>
-        <WrapperComponent someUniqueValue={ 2 }/>
-        <WrapperComponent someUniqueValue={ 3 }/>
-    </ApolloHooksProvider>,
-    document.getElementById('root'),
+  <Provider value={client}>
+    {/*There is no error with one children. The more you add, the higher the chance*/}
+    {/*1 component: no warning*/}
+    {/*2 components: sometimes it gives a warning*/}
+    {/*3 components: random 1 to 3 warnings*/}
+    <WrapperComponent someUniqueValue={1} />
+    <WrapperComponent someUniqueValue={2} />
+    <WrapperComponent someUniqueValue={3} />
+  </Provider>,
+  document.getElementById("root")
 );
